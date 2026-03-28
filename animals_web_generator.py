7b091e58fdea9705880e63c83adb6bcd905efade
Martin Gorren <Martin.Gorren@outlook.com>
2026-03-28 20:28:20 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,28 +1,41 @@
 import json
 
 file_path="c:/user/marti/PycharmProject/Zootopia/animals_data.json"
-def load_data(file_path):
+
+def load_animals_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-animals_data = load_data("animals_data.json")
+def load_html_file(file_path):
+    with open(file_path, "r") as handle:
+        return handle.read()
 
+def write_to_new_html_file(content):
+    with open("animals.html", "w") as f:
+        f.write(content)
 
-for fox in animals_data:
-    #print(f"name in fox: {'name' in fox}")
-    #print(f"characteristics in fox: {'characteristics' in fox}")
-    #print(f"locations in fox: {'locations' in fox}")
-    #for items in fox['characteristics']:
-    #    print(f"type in fox[characteristics]: {'type' in items}")
+def main():
+    animals_data = load_animals_data("animals_data.json")
+    html_data=load_html_file("animals_template.html")
+    print(type(html_data))
+    __replace__= "__REPLACE_ANIMALS_INFO__"
+    animal_repository_string=""
 
-    if "name" in fox:
-        print(f"Name: {fox["name"]}")
-    if "characteristics" in fox:
-        if "diet" in fox["characteristics"]:
-            print(f"Diet: {fox["characteristics"]["diet"]}")
-    if "locations" in fox:
-        print(f"Location: {fox["locations"][0]}")
-    if "characteristics" in fox:
-        if "type" in fox["characteristics"]:
-            print(f"Type: {fox["characteristics"]["type"]}")
-    print("======================")
+    for fox in animals_data:
+        if "name" in fox:
+            animal_repository_string+=f"Name: {fox['name']}\n"
+        if "characteristics" in fox:
+            if "diet" in fox["characteristics"]:
+                animal_repository_string += f"Diet: {fox["characteristics"]["diet"]}\n"
+        if "locations" in fox:
+            animal_repository_string += f"Location: {fox["locations"][0]}\n"
+        if "characteristics" in fox:
+            if "type" in fox["characteristics"]:
+                animal_repository_string += f"Type: {fox["characteristics"]["type"]}\n"
+    html_data=html_data.replace(__replace__,animal_repository_string)
+
+    write_to_new_html_file(html_data)
+    #print(html_data)
+
+if __name__ == "__main__":
+    main()
