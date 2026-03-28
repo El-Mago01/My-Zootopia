ad14ef955b9947693527d3ed23d3b7585adf843e
Martin Gorren <Martin.Gorren@outlook.com>
2026-03-28 21:28:23 +0100

    improved card layout in html regarding list

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -14,34 +14,47 @@ def write_to_new_html_file(content):
     with open("animals.html", "w") as f:
         f.write(content)
 
+def serialize_animal(fox):
+    animal_repository_str=''
+    animal_repository_str += f'<li class="cards__item">\n'
+    animal_repository_str += f'<div class="card__title">\n'
+    if "name" in fox:
+        animal_repository_str += f'{fox['name']}</div>\n'
+        animal_repository_str += f'<div class="card__text">\n'
+        animal_repository_str += f'<ul>\n'
+    if "taxonomy" in fox:
+        if "scientific_name" in fox["taxonomy"]:
+            animal_repository_str += f"<li><strong>Scientific name:</strong> {fox["taxonomy"]["scientific_name"]}</li>\n"
+    if "characteristics" in fox:
+        if "diet" in fox["characteristics"]:
+            animal_repository_str += f"<li><strong>Diet:</strong> {fox["characteristics"]["diet"]}</li>\n"
+    if "locations" in fox:
+        animal_repository_str += f"<li><strong>Location:</strong> {fox["locations"][0]}</li>\n"
+    if "characteristics" in fox:
+        if "type" in fox["characteristics"]:
+            animal_repository_str += f"<li><strong>Type:</strong> {fox["characteristics"]["type"]}</li>\n"
+            animal_repository_str += '</ul>\n'
+    #animal_repository_str+='</p>\n'
+    animal_repository_str+='</li>\n'
+    return animal_repository_str
+
+
 def main():
     animals_data = load_animals_data("animals_data.json")
     html_data=load_html_file("animals_template.html")
     __replace__= "__REPLACE_ANIMALS_INFO__"
-    animal_repository_string=""
 
+
+    fox_repository_string=""
+
+    # for each type of fox in the input json file, create a repository string
     for fox in animals_data:
-        animal_repository_string += f'<li class="cards__item">\n'
-        animal_repository_string += f'<div class="card__title">\n'
-        if "name" in fox:
-            animal_repository_string += f'{fox['name']}</div>\n'
-            animal_repository_string += f'<p class="card__text">\n'
-        if "characteristics" in fox:
-            if "diet" in fox["characteristics"]:
-                animal_repository_string += f"<strong>Diet:</strong> {fox["characteristics"]["diet"]}<br/>\n"
-        if "locations" in fox:
-            animal_repository_string += f"<strong>Location:</strong> {fox["locations"][0]}<br/>\n"
-        if "characteristics" in fox:
-            if "type" in fox["characteristics"]:
-                animal_repository_string += f"<strong>Type:</strong> {fox["characteristics"]["type"]}<br/>\n"
-                animal_repository_string += '</li>\n'
-        animal_repository_string+='</p>\n'
-        animal_repository_string+='</li>\n'
+        fox_repository_string+=serialize_animal(fox)
+
     # The replacement below is necessary to avoid a mojibake
-    animal_repository_string=animal_repository_string.replace("â€™","\'")
-    #print(animal_repository_string)
+    fox_repository_string=fox_repository_string.replace("â€™","\'")
     # Replace the string to replace in the html with the animal repository
-    html_data=html_data.replace(__replace__,animal_repository_string)
+    html_data=html_data.replace(__replace__, fox_repository_string)
 
     write_to_new_html_file(html_data)
     #print(html_data)
