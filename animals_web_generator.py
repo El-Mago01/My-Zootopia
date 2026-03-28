8a7dddd12f2936f3bcc051e9ee5b55ac94aecac2
Martin Gorren <Martin.Gorren@outlook.com>
2026-03-28 19:55:40 +0100

    access data from json file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,1 +1,28 @@
-print("Hello GitHub")
+import json
+
+file_path="c:/user/marti/PycharmProject/Zootopia/animals_data.json"
+def load_data(file_path):
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+animals_data = load_data("animals_data.json")
+
+
+for fox in animals_data:
+    #print(f"name in fox: {'name' in fox}")
+    #print(f"characteristics in fox: {'characteristics' in fox}")
+    #print(f"locations in fox: {'locations' in fox}")
+    #for items in fox['characteristics']:
+    #    print(f"type in fox[characteristics]: {'type' in items}")
+
+    if "name" in fox:
+        print(f"Name: {fox["name"]}")
+    if "characteristics" in fox:
+        if "diet" in fox["characteristics"]:
+            print(f"Diet: {fox["characteristics"]["diet"]}")
+    if "locations" in fox:
+        print(f"Location: {fox["locations"][0]}")
+    if "characteristics" in fox:
+        if "type" in fox["characteristics"]:
+            print(f"Type: {fox["characteristics"]["type"]}")
+    print("======================")
