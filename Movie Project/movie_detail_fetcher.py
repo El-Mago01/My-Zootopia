478ceb1da8acdb31d1d25adbbde0a0bdeed6c1bd
marti <Martin.Gorren@Outlook.com>
2026-04-27 01:34:32 +0200

    Used autopep8 to clean the code

--- Movie Project/movie_detail_fetcher.py
+++ Movie Project/movie_detail_fetcher.py
@@ -7,18 +7,22 @@ load_dotenv()
 
 BASE_URL = "https://www.omdbapi.com/"
 
-api_key=os.getenv('apikey')
+api_key = os.getenv('apikey')
 
-CURRENT_MOVIE={}
+CURRENT_MOVIE = {}
 
-def get_movie_data(imdbID:str="", title:str="") -> dict:
+
+def get_movie_data(imdbID: str = "", title: str = "") -> dict:
     print(f"imdbID={imdbID}, title={title}")
-    def specify_search_term(imdbID:str="0", title:str="")-> str:
+
+    def specify_search_term(imdbID: str = "0", title: str = "") -> str:
         try:
-            if imdbID != "": # the imdbID is the main search key
+            if imdbID != "":  # the imdbID is the main search key
                 search_term = f"?apikey={api_key}&i={imdbID}"
-                print(f"imdbID={imdbID} provided. This is the search term:", search_term)
-            elif title!="": # the title is the main search key
+                print(
+                    f"imdbID={imdbID} provided. This is the search term:",
+                    search_term)
+            elif title != "":  # the title is the main search key
                 search_term = f"?apikey={api_key}&s={title}"
                 print("title provided. This is the search term:", search_term)
             return search_term
@@ -29,10 +33,10 @@ def get_movie_data(imdbID:str="", title:str="") -> dict:
             return search_term
 
     if imdbID != "":  # the imdbID is the main search key
-       url = BASE_URL + specify_search_term(imdbID,"")
-       print(url)
-    elif title!="":    # the title is the main search key
-        url=BASE_URL+specify_search_term("",title)
+        url = BASE_URL + specify_search_term(imdbID, "")
+        print(url)
+    elif title != "":    # the title is the main search key
+        url = BASE_URL + specify_search_term("", title)
         print(url)
     try:
         response = requests.get(url)
@@ -42,31 +46,33 @@ def get_movie_data(imdbID:str="", title:str="") -> dict:
         print("Please contact your system administrator")
         movie_details = {}
         return movie_details
-    if movie_details['Response'] == "False": # the json contains an invalid response
+    if movie_details['Response'] == "False":  # the json contains an invalid response
         print("Movie not found!.", movie_details['Error'])
         movie_details = {}
     else:
         print("Valid response received: ", movie_details['Response'])
-    return  movie_details
+    return movie_details
 
-def fetch_movie_general_data(title:str)-> list:
-    movie_data=get_movie_data("",title)
+
+def fetch_movie_general_data(title: str) -> list:
+    movie_data = get_movie_data("", title)
     print(movie_data)
     print(type(movie_data))
-    if len(movie_data)>0:
+    if len(movie_data) > 0:
         return movie_data['Search']
     else:
         return []
 
 
-def fetch_specific_movie_details(imdbID:str)-> dict:
-    movie_data=get_movie_data(imdbID)
+def fetch_specific_movie_details(imdbID: str) -> dict:
+    movie_data = get_movie_data(imdbID)
     return movie_data
 
-def fetch_specific_movie_detail_item(item:str, imdbID:str) -> str:
+
+def fetch_specific_movie_detail_item(item: str, imdbID: str) -> str:
     try:
         movie_details = fetch_specific_movie_details(imdbID)
-        print("returning for item:" ,movie_details[item])
+        print("returning for item:", movie_details[item])
         if movie_details[item] is not None:
             return movie_details[item]
         else:
@@ -75,17 +81,20 @@ def fetch_specific_movie_detail_item(item:str, imdbID:str) -> str:
         print("Invalid item received: ", movie_details)
         return ""
 
+
 def main():
-    title="matrix"
-    movie_details=fetch_movie_general_data(title)
+    title = "matrix"
+    movie_details = fetch_movie_general_data(title)
     print(f"Showing you now {len(movie_details)} movies")
     for movie in movie_details:
-        print(f"{movie['imdbID']:<15}  - {movie['Title']:<40} - {movie['Year']:<10}")
-    imdbID="tt10838180"
-    movie_details=fetch_specific_movie_details(imdbID) #Best film EVER!!! :-)
+        print(
+            f"{movie['imdbID']:<15}  - {movie['Title']:<40} - {movie['Year']:<10}")
+    imdbID = "tt10838180"
+    movie_details = fetch_specific_movie_details(
+        imdbID)  # Best film EVER!!! :-)
     print(movie_details)
     try:
-        year=int(fetch_specific_movie_detail_item('Year', imdbID))
+        year = int(fetch_specific_movie_detail_item('Year', imdbID))
     except (ValueError, TypeError):
         return
     print(year)
@@ -93,4 +102,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- Movie Project/movie_storage_sql.py
+++ Movie Project/movie_storage_sql.py
@@ -1,8 +1,8 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text
 
 # Define the database URL
 DB_URL = "sqlite:///movies.db"
-DEBUG=True
+DEBUG = True
 # Create the engine
 engine = create_engine(DB_URL, echo=DEBUG)
 
@@ -20,30 +20,41 @@ with engine.connect() as connection:
     """))
     connection.commit()
 
+
 def list_movies():
     """Retrieve all movies from the database."""
     with engine.connect() as connection:
-        result = connection.execute(text("SELECT id, imdbID, Title, Year, imdbRating, Poster FROM movies"))
+        result = connection.execute(
+            text("SELECT id, imdbID, Title, Year, imdbRating, Poster FROM movies"))
         movies = result.fetchall()
-    print (movies)
+    print(movies)
     print(type(movies))
     return movies
 
-def add_movie(movie:dict) -> bool:
+
+def add_movie(movie: dict) -> bool:
     """Add a movie to the database."""
 
-    imdbID=movie['imdbID']
-    title=movie['Title']
-    year=movie['Year']
-    rating=movie['Rating']
-    poster=movie['Poster']
+    imdbID = movie['imdbID']
+    title = movie['Title']
+    year = movie['Year']
+    rating = movie['Rating']
+    poster = movie['Poster']
     print("Poster:", poster)
     with engine.connect() as connection:
         try:
             print(f"Inserting movie {title} into the database")
-            params={"imdbID": imdbID, "title": title, "year": year, "rating": rating, "poster": poster}
-            connection.execute(text("INSERT INTO movies (imdbID, Title, Year, imdbRating, Poster) "
-                                    "VALUES (:imdbID, :title, :year, :rating, :poster)"), params)
+            params = {
+                "imdbID": imdbID,
+                "title": title,
+                "year": year,
+                "rating": rating,
+                "poster": poster}
+            connection.execute(
+                text(
+                    "INSERT INTO movies (imdbID, Title, Year, imdbRating, Poster) "
+                    "VALUES (:imdbID, :title, :year, :rating, :poster)"),
+                params)
             connection.commit()
             print(f"Movie '{title}' added successfully.")
             return True
@@ -51,11 +62,13 @@ def add_movie(movie:dict) -> bool:
             print(f"Error: {e}")
             return False
 
+
 def delete_movie(movie_id, title) -> bool:
     """Delete a movie from the database."""
     with engine.connect() as connection:
         try:
-            print(f"Deleting movie {title} with ID {movie_id} from the database")
+            print(
+                f"Deleting movie {title} with ID {movie_id} from the database")
             connection.execute(text("DELETE FROM movies WHERE ID = :id"),
                                {"id": movie_id})
             connection.commit()
@@ -85,30 +98,29 @@ def delete_movie(movie_id, title) -> bool:
 #   match_type 1 => exact match but case-insensitive
 #   match_type 2 => matching characters, but case-insensitive and stripped
 #   match_type 3 => fuzzy matching
-def search_movie(title,match_type:int=0) -> dict:
+def search_movie(title, match_type: int = 0) -> dict:
     """Search for a movie from the database."""
 
-    SEARCH_QUERY_0="SELECT id,imdbID, title, year, imdbRating,poster FROM movies WHERE title = :title"
-    params_0={"title": title}
-    SEARCH_QUERY_1="SELECT id,imdbID, title, year, imdbRating, poster FROM movies WHERE LOWER(title) = :title"
-    params_1={"title": title.lower()}
-    SEARCH_QUERY_2="SELECT id,imdbID, title, year, imdbRating, poster FROM movies WHERE REPLACE(LOWER(title),' ','') = :title"
-    params_2={"title": title.lower().replace(' ','')}
-    SEARCH_QUERY_3="SELECT id,imdbID, title, year, imdbRating, poster FROM movies WHERE LOWER(title) LIKE :title"
-    params_3={"title": f"%{title.lower()}%"}
-
+    SEARCH_QUERY_0 = "SELECT id,imdbID, title, year, imdbRating,poster FROM movies WHERE title = :title"
+    params_0 = {"title": title}
+    SEARCH_QUERY_1 = "SELECT id,imdbID, title, year, imdbRating, poster FROM movies WHERE LOWER(title) = :title"
+    params_1 = {"title": title.lower()}
+    SEARCH_QUERY_2 = "SELECT id,imdbID, title, year, imdbRating, poster FROM movies WHERE REPLACE(LOWER(title),' ','') = :title"
+    params_2 = {"title": title.lower().replace(' ', '')}
+    SEARCH_QUERY_3 = "SELECT id,imdbID, title, year, imdbRating, poster FROM movies WHERE LOWER(title) LIKE :title"
+    params_3 = {"title": f"%{title.lower()}%"}
 
     with engine.connect() as connection:
         try:
             print(f"Searching for movie {title} into the database")
             if match_type == 0:
-                result=connection.execute(text(SEARCH_QUERY_0), params_0)
+                result = connection.execute(text(SEARCH_QUERY_0), params_0)
             elif match_type == 1:
-                result=connection.execute(text(SEARCH_QUERY_1),params_1)
+                result = connection.execute(text(SEARCH_QUERY_1), params_1)
             elif match_type == 2:
-                result=connection.execute(text(SEARCH_QUERY_2),params_2)
+                result = connection.execute(text(SEARCH_QUERY_2), params_2)
             elif match_type == 3:
-                result=connection.execute(text(SEARCH_QUERY_3),params_3)
+                result = connection.execute(text(SEARCH_QUERY_3), params_3)
             else:
                 raise ReferenceError("Search function not implemented")
             connection.commit()
@@ -116,7 +128,7 @@ def search_movie(title,match_type:int=0) -> dict:
         except Exception as e:
             print(f"Error: {e}")
             return {}
-        res=result.fetchall()
+        res = result.fetchall()
         print(f"the search function will return this: {res}")
 
         print(type(res))
@@ -131,14 +143,15 @@ def main():
     # found_movies=search_movie("The hulk",1)
     # print(f" This/these movie(s) were found{found_movies}")
 
-    found_movies=search_movie("    The    hulk   ",2)
+    found_movies = search_movie("    The    hulk   ", 2)
     print(f" This/these movie(s) were found{found_movies}")
 
-    found_movies=search_movie("hulk",3)
+    found_movies = search_movie("hulk", 3)
     print(f" This/these movie(s) were found{found_movies}")
 
-    found_movies=search_movie("Bee Bulk",4)
+    found_movies = search_movie("Bee Bulk", 4)
     print(f" This/these movie(s) were found{found_movies}")
+
 
 if __name__ == '__main__':
     main()
